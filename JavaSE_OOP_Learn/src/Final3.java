d9ff049b728cea47c97d142d80e8b9f0cbedd838
debugyes <1075292937@qq.com>
2020-03-30 12:48:17 +0800

    添加

--- JavaSE_OOP_Learn/src/Final3.java
+++ JavaSE_OOP_Learn/src/Final3.java
@@ -3,10 +3,10 @@
     基本类型：基本类型的值不能改变
     引用类型：引用类型的地址值不能发生改变，但是，该对象的堆内存的值是可以改变的
  */
-public class final3 {
-    public static void main(String[] args){
+public class Final3 {
+    public static void main(String[] args) {
         //局部变量是基本数据类型
-        int x = 10;
+        int x;
         x = 100;
         System.out.println(x);
         final int y = 10;
