12f9740a55e8224bf30175c4f5afdb3425209802
debugyes <1075292937@qq.com>
2020-04-21 21:41:20 +0800

    update

--- Learning/src/JavaSE_OOP_Learn/src/AbstractClass3.java
+++ Learning/src/JavaSE_OOP_Learn/src/AbstractClass3.java
@@ -24,7 +24,6 @@ package JavaSE_OOP_Learn.src;
             构造方法：无参，带参
             成员方法：吃饭（）
 
-
     实现：从抽象到具体
         动物类：
             成员变量：姓名，年龄，
@@ -41,10 +40,93 @@ package JavaSE_OOP_Learn.src;
 
 public class AbstractClass3 {
     public static void main(String[] args) {
+        //测试狗类
+        //方式1:
+        DogC d = new DogC();
+        d.setName("旺财");
+        d.setAge(3);
+        System.out.println(d.getName() + "---" + d.getAge());
+        //测试2:
+        DogC d2 = new DogC("旺财", 3);
+        System.out.println(d.getName() + "---" + d.getAge());
+        d2.eat();
+        System.out.println("-----------------------------------");
+
+        AnimalC a = new DogC();
+        a.setAge(3);
+        a.setName("旺财");
+        System.out.println(a.getAge() + "---" + a.getName());
+        a.eat();
+
+        AnimalC a2 = new DogC("旺财", 3);
+        System.out.println(a2.getAge() + "---" + a2.getName());
+        a2.eat();
+
+
+        //测试猫类
 
     }
 }
 
 abstract class AnimalC {
+    private String name;
+    private int age;
 
+    public AnimalC() {
+
+    }
+
+    public AnimalC(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public abstract void eat();
 }
+
+//定义具体狗类
+class DogC extends AnimalC {
+    public DogC() {
+
+    }
+
+    public DogC(String name, int age) {
+        super(name, age);
+    }
+
+    public void eat() {
+        System.out.println("狗吃肉");
+    }
+}
+
+//定义猫类
+class CatC extends AnimalC {
+    public CatC() {
+
+    }
+
+    public CatC(String name, int age) {
+        super(name, age);
+    }
+
+    public void eat() {
+        System.out.println("猫吃鱼");
+    }
+}
+
